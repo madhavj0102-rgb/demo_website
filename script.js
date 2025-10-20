0fac257ab3fa7cfeb4959314a5d95b639a740ee5
madhavj0102-rgb <madhavj0102@gmail.com>
2025-10-20 10:19:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function submitForm() {
+    const selected = document.querySelector('input[name="rel"]:checked');
+
+    if (!selected) {
+        alert("Please select any one option before submitting!");
+        return;
+    }
+
+    // Redirect based on value
+    if (selected.value === "bhau") {
+        window.location.href = "bhau.html";
+    } 
+    else if (selected.value === "friend") {
+        window.location.href = "friend.html";
+    } 
+    else if (selected.value === "student") {
+        window.location.href = "student.html";
+    }
+    else if (selected.value === "son") {
+        window.location.href = "son.html";
+    }
+    else if (selected.value === "nephew") {
+        window.location.href = "nephew.html";
+    }
+    else if (selected.value === "partner") {
+        window.location.href = "life_partner.html";
+    }
+}
+
